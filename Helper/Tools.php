67fb32f4d2c4ec3a3716318d9a7dce113ced9ace
david-fiaty-cko <david.fiaty@checkout.com>
2018-08-22 18:32:20 +0100

    Removed unused request class

--- Helper/Tools.php
+++ Helper/Tools.php
@@ -11,7 +11,6 @@
 namespace CheckoutCom\Magento2\Helper;
 
 use Magento\Framework\Message\ManagerInterface;
-use Magento\Framework\App\Request\Http;
 use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Customer\Model\Session as CustomerSession;
 use Magento\Framework\UrlInterface;
@@ -29,11 +28,6 @@ class Tools {
     const KEY_PUBLIC_KEY = 'public_key';
     const KEY_PRIVATE_KEY = 'private_key';
     const KEY_PRIVATE_SHARED_KEY = 'private_shared_key';
-
-    /**
-     * @var Http
-     */
-    protected $request;
 
     /**
      * @var ScopeConfigInterface
@@ -59,13 +53,11 @@ class Tools {
      * Tools constructor.
      */ 
     public function __construct(
-        Http $request,
         ScopeConfigInterface $scopeConfig,
         CustomerSession $customerSession,
         UrlInterface $urlInterface,
         Reader $directoryReader
     ) {
-        $this->request           = $request;
         $this->scopeConfig       = $scopeConfig;
         $this->customerSession   = $customerSession;
         $this->urlInterface      = $urlInterface;
@@ -163,4 +155,12 @@ class Tools {
             $this->customerSession->authenticate();
         }    
     }
+
+    /**
+     * Returns a prepared post response.
+     */    
+    public function getPostResponse($url, $params) {
+
+    }
+
 }
